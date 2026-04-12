1cbb32b82545b63e08f213596f1cc6c0b27d5924
Maryam Shanabli <148885553+MaryamShanabli@users.noreply.github.com>
2026-04-12 20:30:28 +0300

    Adjust env/export visibility behavior

--- builtin_io.c
+++ builtin_io.c
@@ -6,7 +6,7 @@
 /*   By: mshanabl <mshanabl@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 20:04:11 by mshanabl          #+#    #+#             */
-/*   Updated: 2026/04/11 13:15:43 by mshanabl         ###   ########.fr       */
+/*   Updated: 2026/04/12 20:17:06 by mshanabl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,14 +53,30 @@ int	builtin_env(t_cmd *cmd)
 {
 	extern char	**environ;
 	int			i;
+	int			has_equal;
+	int			j;
 
 	if (cmd->argv[1])
 		return (error_msg(1, "env", NULL, "no options or arguments supported"));
 	i = 0;
 	while (environ[i])
 	{
-		write(1, environ[i], ft_strlen(environ[i]));
-		write(1, "\n", 1);
+		has_equal = 0;
+		j = 0;
+		while (environ[i][j])
+		{
+			if (environ[i][j] == '=')
+			{
+				has_equal = 1;
+				break ;
+			}
+			j++;
+		}
+		if (has_equal)
+		{
+			write(1, environ[i], ft_strlen(environ[i]));
+			write(1, "\n", 1);
+		}
 		i++;
 	}
 	return (0);
